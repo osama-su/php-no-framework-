0ebc221169e2ea07affe61b826404a8ec7cece48
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-01-18 17:40:42 +0200

    initialing

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php 
+declare(strict_types=1);
+use DI\ContainerBuilder;
+use ExampleApp\HelloWorld;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Relay\Relay;
+use Laminas\Diactoros\ServerRequestFactory;
+use function DI\create;
+use function FastRoute\simpleDispatcher;
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$routes = simpleDispatcher(function (RouteCollector $r) {
+    $r->get('/hello', HelloWorld::class);
+});
+
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler();
+
+$requestHandler = new Relay($middlewareQueue);
+$requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$helloWorld = $container->get(HelloWorld::class);
+$helloWorld->__invoke();
+
+
+?>
---
+++ src/helloworld.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace ExampleApp;
+
+class HelloWorld
+{
+     public function __invoke(): void
+    {
+        echo 'Hello, autoloaded world!';
+        exit;
+    }
+}
